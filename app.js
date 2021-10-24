e0ed3179f79648f7be4b650ec521528ffc948a94
malik306 <mubashir.official@gmail.com>
2021-10-24 22:03:34 +0500

    some services added

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
+const el = document.querySelector('.nav__bar__burger');
 function ToggleMenu() {
-    const el = document.querySelector('.nav__bar__burger');
     el.classList.toggle('nav__bar__burger--toggle');
+    el.nextElementSibling.classList.toggle('nav__bar__list--toggle');
 }
+window.addEventListener('scroll',()=>{
+    if (window.scrollY>0) {
+        el.parentElement.style = `background: #00BCD4`;
+    } else {
+        el.parentElement.style = ``;
+    }
+});
