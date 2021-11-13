d78412b37576b4d553382e391c569149ae5d8652
malik306 <mubashir.official@gmail.com>
2021-11-13 12:53:06 +0500

    nav menu responsive bug fixed

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const el = document.querySelector('.nav__bar__burger');
+const jumpScrollUp = document.querySelector('.scroll-y');
 function ToggleMenu() {
     el.classList.toggle('nav__bar__burger--toggle');
     el.nextElementSibling.classList.toggle('nav__bar__list--toggle');
@@ -6,7 +7,9 @@ function ToggleMenu() {
 window.addEventListener('scroll',()=>{
     if (window.scrollY>0) {
         el.parentElement.style = `background: #00BCD4`;
+        jumpScrollUp.style = `display: unset`;
     } else {
         el.parentElement.style = ``;
+        jumpScrollUp.style = `display: none`;
     }
 });
